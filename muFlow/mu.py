d3eea0fb92be80616aa1560010abceb01c79c7dd
Przemysław Dolata <snowball91b@gmail.com>
2018-06-26 13:53:39 +0200

    simpler argument parsing

--- muFlow/mu.py
+++ muFlow/mu.py
@@ -6,32 +6,29 @@ from baseTasks import muException
 
 if __name__ == "__main__":
   p = argparse.ArgumentParser(description='muFlow, the parallel processing engine')
-  p.add_argument('script', help='Script to run')
+  p.add_argument('script', nargs='?', default=None, help='Script to run')
   p.add_argument('--num-processes', '-n', type=int, default=0,
       help='Number of processes to spawn (default is CPU count)')
   p.add_argument('--debug', action='store_true',
       help='Debug mode: each parallel task processes only the first item in a single process')
-  p.add_argument('--list', action='store_true', help='Print list of available tasks')
-  p.add_argument('--info', type=str, help='Print more detailed info on a specific task')
+  p.add_argument('--info', type=str, nargs='?', const='', default=None,
+      help='Print list of available tasks, OR, if followed by a name of task, details of this task.')
   p.add_argument('--no-vt', action='store_true', help='Do not use VT100 escape codes')
   p.add_argument('--no-log', action='store_true', help='Do not print logs and time measurements')
 
   asm = Assembler()
 
-  if '--list' in sys.argv:
-    asm.printInfo()
-    exit()
-  if '--info' in sys.argv:
-    info = sys.argv[sys.argv.index('--info') + 1]
-    asm.printInfo(info)
-    exit()
-
   args = p.parse_args()
-
   if args.no_vt:
     asm.preventVT100()
   if args.no_log:
     asm.preventLogging()
+  if args.info is not None:
+    asm.printInfo(args.info if args.info != '' else None)
+    exit()
+  if args.script is None:
+    print("You are supposed to pass a path to the script file as the first positional argument.")
+    exit()
 
   with open(args.script, 'r') as script_file:
     script = [line.strip('\n') for line in script_file.readlines()]
